6cebb04dfc6a21e8ba22d495e82556b936c2c567
Hamad Hamdi <Hammock42@protonmail.com>
2024-08-15 15:12:44 +0200

    Update Settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yor)1vku-#mx1sa0$m68hav42nt$l&j7&k2f2c#aaqpe%d@zn1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['PythonAnywhere.com', 'localhost']
 
 
 # Application definition
